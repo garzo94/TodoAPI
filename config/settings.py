03a873a10cf7d7be021f895d22bc38e89cc86b7c
garzo94 <alexgarzo25@gmail.com>
2022-01-12 08:57:37 -0600

    final commit

--- config/settings.py
+++ config/settings.py
@@ -38,19 +38,37 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #3rd Party
+    'rest_framework',
+    'corsheaders',
+
     #Local
     'todos',
 ]
 
+REST_FRAMEWORK = {
+'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# new
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
 
 ROOT_URLCONF = 'config.urls'
 
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+class ListTodo(generics.ListAPIView): #ListAPIView to display all todos
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView): #RetrieveAPIView to to get just one Todo
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
